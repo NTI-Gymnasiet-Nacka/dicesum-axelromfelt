20ded973ef2714bac0b41bb723abb8de8e709522
axelromfelt <114027887+axelromfelt@users.noreply.github.com>
2024-08-20 10:53:26 +0200

    start

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # Dice sum probability calculator
-# Författare: 
+# Författare:
 # Datum:
 
 def main():
     user_input = input().split(" ")
+    for i in range(len(user_input)):
+        user_input[i] = int(user_input[i])
+    results = []
+    for i in range(user_input[0]):
+        for j in range(user_input[1]):
+            results.append(i+j)
+    print(results)
+
 
 if __name__ == "__main__":
     main()
